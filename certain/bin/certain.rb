18de683b4a7fdfd1d1ed9c88b6a16ca99cf18bf1
FOXX <siliconfoxx@gmail.com>
2015-10-07 13:45:55 -0500

    Added IWAD, GameServer initialization.

--- certain/bin/certain.rb
+++ certain/bin/certain.rb
@@ -10,7 +10,7 @@ require_relative "../lib/libcertain.rb"
 require "optparse"
 
 # Default argument values
-options = {:websocketport => '5238', :wadfiles => [], :assets => ['hackdoom.pk3'], :verbose => 'false'}
+options = {:websocketport => '5238', :wadfiles => [], :assets => ['hackdoom.pk3'], :iwad => "doom2.wad", :verbose => 'false'}
 
 # Check number of arguments and for if the user requests help
 parser = OptionParser.new do |o|
@@ -18,6 +18,7 @@ parser = OptionParser.new do |o|
   o.on("--websocketport PORT", "Port number to present via websocket") { |i| options[:websocketport] = i }
   o.on("--wadfiles A,B,C[...]", Array, "WAD files to run the server with") { |wads| options[:wadfiles] = wads }
   o.on("--assets D,E,F[...]", Array, "Asset files to load in Certain") { |files| options[:assets] = files }
+  o.on("--iwad IWAD", "IWAD file (doom2.wad, doom.wad, etc") { |iwad| options[:iwad] = iwad }
   o.on( '-v', '--verbose', 'Output debugging information' ) { options[:verbose] = true}
   o.on('-h') { puts o; exit }
 end
@@ -25,12 +26,16 @@ parser.parse!
 
 puts "Certain Doom launching..."
 
-# Check for verbosity
+# Check for verbosity and dump startup options
 if options[:verbose] == true then
   puts "Websocket port:  #{options[:websocketport]}"
+  puts "IWAD:  #{options[:iwad]}"
   puts "WADs:\n"
     unless options[:wadfiles].empty? then options[:wadfiles].each { |i| puts " -#{i}" } end
   puts "Assets:\n"
     unless options[:assets].empty? then options[:assets].each { |i| puts " -#{i}" } end
   puts "Verbose?  #{options[:verbose]}"
 end
+
+# Initialize GameServer object
+gameServer = GameServer.new(options[:iwad], options[:assets], options[:wadfiles])
--- certain/lib/libcertain.rb
+++ certain/lib/libcertain.rb
@@ -1,15 +1,42 @@
 # GameServer defines a running game server
 class GameServer
+  def initialize(iwad, assets, wads)
+    @zdoom = `which zdoom`.strip!
+    @args = "-iwad #{iwad} -file #{assets.gsub(/,/, ' ')} #{wads.gsub(/,/, ' ')}"
+  end
+
+  def where?
+    @zdoom
+  end
+
+  def start
+    `#{@zdoom} #{@args}`
+  end
+
+  def stop
+    pid = `ps aux | grep #{@zdoom}`
+    `kill #{pid}`
+  end
 end
 
 # GameEvent is the base class that defines communication to and from a game in progress
 class GameEvent
+  def intialize
+  end
 end
 
 # GameCommand is for events originating within a game
 class GameCommand < GameEvent
+  def initialize
+  end
 end
 
 # ExternalCommand is for events originating outside the game
 class ExternalCommand < GameEvent
+  def initialize
+  end
+
+  def openHackdoor (door)
+    puts "pukename HackDoom OpenHackdoor #{door}"
+  end
 end
