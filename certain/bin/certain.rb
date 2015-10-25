f35462aff74818fd944e6477640ca535791ebbb9
FOXX <siliconfoxx@gmail.com>
2015-10-25 16:26:06 -0500

    Dropped io from pukename test

--- certain/bin/certain.rb
+++ certain/bin/certain.rb
@@ -46,4 +46,4 @@ gameServer = GameServer.new(options[:iwad], options[:assets], options[:wadfiles]
 # Run the server
 gameServer.async.start
 
-loop do sleep 10 ; io.puts "pukename \"HackDoom Welcome\"" end
+loop do sleep 10 ; $stdout.puts "pukename \"HackDoom Welcome\"" end
