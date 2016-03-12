72bf84a79fd206a40e8d2eb9806429792ee9d13e
FOXX <siliconfoxx@gmail.com>
2016-03-11 22:10:42 -0600

    Added hackdoom-client.sh handy launching script

---
+++ certain/examples/hackdoom-client.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+/usr/local/games/zandronum/zandronum -iwad ~/.zandronum/doom2.wad -file ~/repos/hack-doom/assets/hackdoom.pk3 ~/repos/hack-doom/levels/hackdoom001/hackdoom001.wad +connect localhost
