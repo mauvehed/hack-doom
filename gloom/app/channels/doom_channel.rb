737cc4d2c28cda6eb584664c8021741c6b01db0b
FOXX <siliconfoxx@gmail.com>
2016-04-03 13:04:34 -0500

    Typo

--- gloom/app/channels/doom_channel.rb
+++ gloom/app/channels/doom_channel.rb
@@ -11,10 +11,9 @@ class DoomChannel < ApplicationCable::Channel
   end
 
   def startserver
-    servercheck = ""
-    servercheck = `ps aux |grep \"#{command}\" | grep -v grep`
-
     command = "~/repos/hack-doom/certain/bin/certain.rb --iwad ~/.zandronum/doom2.wad --wadfiles ~/repos/hack-doom/levels/hackdoom001/hackdoom001.wad --assets ~/repos/hack-doom/assets/hackdoom.pk3 --level hack01 --marines 2"
+    servercheck = ""
+    servercheck = `ps aux |grep "#{command}" | grep -v grep`
 
     # Do a check to make sure the server's not already running
     if servercheck == "" then
