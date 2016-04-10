06518d5400dea572ac9775abd5f28e71ebc9217b
FOXX <siliconfoxx@gmail.com>
2016-04-10 16:10:36 -0500

    Added Challenge and Score models

--- gloom/db/schema.rb
+++ gloom/db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160405051455) do
+ActiveRecord::Schema.define(version: 20160410210743) do
+
+  create_table "challenges", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "area"
+    t.text     "flag"
+    t.integer  "difficulty"
+    t.string   "type"
+    t.text     "hint"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "doom_logs", force: :cascade do |t|
     t.text     "content"
@@ -19,8 +30,23 @@ ActiveRecord::Schema.define(version: 20160405051455) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "levels", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "areas"
+    t.text     "hacklifts"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "messages", force: :cascade do |t|
     t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "scores", force: :cascade do |t|
+    t.string   "team"
+    t.integer  "score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
