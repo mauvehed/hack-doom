00d5218af6216a1cc06cae39b62395dda8ab3905
FOXX <siliconfoxx@gmail.com>
2016-04-17 02:29:17 -0500

    Updates for doomservercontroller

--- gloom/app/channels/doom_channel.rb
+++ gloom/app/channels/doom_channel.rb
@@ -71,24 +71,6 @@ class DoomChannel < ApplicationCable::Channel
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def startserver
-    command = "~/repos/hack-doom/certain/bin/certain.rb --iwad ~/.zandronum/doom2.wad --wadfiles ~/repos/hack-doom/levels/hackdoom001/hackdoom001.wad --assets ~/repos/hack-doom/assets/hackdoom.pk3 --level hack01 --marines 2"
-    servercheck = ""
-    servercheck = `ps aux |grep "#{command}" | grep -v grep`
-
-    # Do a check to make sure the server's not already running
-    if servercheck == "" then
-      @inpipe, @outpipe = Open3.popen2 "#{command}"
-
-      # Store output from Certain
-      while res = @outpipe.gets
-        DoomLog.create! content: res
-      end
-    else
-      puts "Server already running."
-    end
-  end
-
   def relay(data)
     #puts "Data in DoomChannel::relay:  #{data}"              #DEBUG
 
@@ -98,8 +80,8 @@ class DoomChannel < ApplicationCable::Channel
     datastring.gsub!(/^\s+/, "")
     #puts "StringData in DoomChannel::relay:  #{datastring}"  #DEBUG
 
-    # Send the string over to Certain
-    @inpipe.puts("#{datastring}")
+    # Send the string over to DoomServerController
+    DoomServerController#update(datastring)
   end
 
   # Change the map
--- gloom/app/controllers/doom_server_controller.rb
+++ gloom/app/controllers/doom_server_controller.rb
@@ -1,2 +1,41 @@
+require 'open3'
+
 class DoomServerController < ApplicationController
+  def index
+    command = "~/repos/hack-doom/certain/bin/certain.rb --iwad ~/.zandronum/doom2.wad --wadfiles ~/repos/hack-doom/levels/hackdoom001/hackdoom001.wad --assets ~/repos/hack-doom/assets/hackdoom.pk3 --level hack01 --marines 2"
+    servercheck = ""
+    servercheck = `ps aux |grep "#{command}" | grep -v grep`
+
+    # Do a check to make sure the server's not already running
+    if servercheck == "" then
+      @inpipe, @outpipe = Open3.popen2 "#{command}"
+
+      # Store output from Certain
+      while res = @outpipe.gets
+        DoomLog.create! content: res
+      end
+    else
+      puts "Server already running."
+    end
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  # Send the string over to Certain
+  def update(message)
+    @inpipe.puts("#{message}")
+  end
+
+  def destroy
+  end
 end
